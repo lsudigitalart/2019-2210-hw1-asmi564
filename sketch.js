65db8112dc2ca53fc4953e7de52de484ff762014
asmith516 <ummally33@gmail.com>
2019-09-03 12:28:45 -0500

    hw1 ally smith

--- sketch.js
+++ sketch.js
@@ -1,11 +1,30 @@
 function setup () {
-    createCanvas(800, 600);
-    background(0,0,0);
-    ellipse(10, 10, 20, 50);
-    ellipse(10, 10, 20, 50);
-
+    createCanvas(700, 700);
+    background(249,233,153);
+    //noStroke();
+  
+    let colorScheme = [
+      color(255,201,201),
+      color(180,210,228),
+      color(204,255,204),
+      color(248,248,248),
+      color(142,66,133),
+    ];
+    
+    drawCluster(100, 100, colorScheme);
+    drawCluster(500, 200, colorScheme);
+    drawCluster(200, 500, colorScheme);
 }
 
 function draw () {
 
+}
+
+function drawCluster(x, y, colors) {
+  var size = 200;
+  for (var i = 5; i >= 1; i--) {
+    fill(random(colors));
+    circle(random(size) + x, random(size) + y, size * 1.2);
+    size -= 40;
+  }
 }
